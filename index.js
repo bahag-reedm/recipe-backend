934d55bea9b56f9ca2a4b1b8a0bee2478c065ee4
Maximilian Reed <maximilian.reed@bahag.com>
2026-03-23 16:20:54 +0100

    added db connection

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
 import "dotenv/config";
-import recipes from "./recipies.json" with { type: "json" };
+import cors from "cors";
+import recipes from "./data/recipes.json" with { type: "json" };
+import { Pool } from "pg";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const pool = new Pool({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  port: process.env.PORT,
+});
+
 app.use(express.json());
+app.use(cors());
 
 app.use(
   express.urlencoded({
@@ -14,24 +25,71 @@ app.use(
 );
 
 app.get("/recipes", (request, response) => {
-  response.send(recipes.recipes);
+  response.send(recipes.meals);
+});
+
+app.get("/orders", async (req, res) => {
+  try {
+    const orders = await pool.query("SELECT * FROM orders");
+    console.log(orders);
+    return res.json(orders.rows);
+  } catch (err) {
+    return res.json(err.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await pool.query("SELECT * FROM users");
+    console.log(users);
+    return res.json(users.rows);
+  } catch (err) {
+    return res.json(err.message);
+  }
 });
 
 app.get("/recipes/:id", (request, response) => {
   let { id } = request.params;
-  const recipe = recipes.recipes.find((e) => e.id === parseInt(id));
+  const recipe = recipes.meals.find((e) => e.idMeal === parseInt(id));
   response.send(recipe);
 });
 
 app.post("/recipes", (request, response) => {
-  let lastID = recipes.recipes.length;
-  console.log(lastID);
+  let lastID = recipes.meals.length;
 
   let recipe = request.body;
   recipe.id = lastID + 1;
 
-  recipes.recipes.push(recipe);
+  recipes.meals.push(recipe);
   response.send(recipes);
+});
+
+app.put("/recipes/:id", (request, response) => {
+  let { id } = request.params;
+  const index = recipes.meals.findIndex((e) => e.idMeal === parseInt(id));
+
+  if (index === -1) {
+    return response.status(404).send({ message: "Recipe not found" });
+  }
+
+  recipes.meals[index] = {
+    ...recipes.meals[index],
+    ...request.body,
+    id: parseInt(id),
+  };
+  response.send(recipes.recipes[index]);
+});
+
+app.delete("/recipes/:id", (request, response) => {
+  let { id } = request.params;
+  const index = recipes.meals.findIndex((e) => e.idMeal === parseInt(id));
+
+  if (index === -1) {
+    return response.status(404).send({ message: "Recipe not found" });
+  }
+
+  const deletedRecipe = recipes.meals.splice(index, 1);
+  response.send(deletedRecipe[0]);
 });
 
 app.listen(PORT, () => {
